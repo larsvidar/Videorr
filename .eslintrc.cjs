b715802be83f7de75e28b49348245a88cfdc80d0
Lars Vidar Nordli <larsvidar@larsvidar.no>
2023-05-20 10:29:33 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+	root: true,
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-typescript',
+	],
+	parserOptions: {ecmaVersion: 'latest'},
+	rules: {
+		'no-console': 'warn',
+		indent: ['warn', 'tab'],
+		semi: ['warn', 'always', {
+			omitLastInOneLineBlock: true,
+		}],
+		quotes: ['warn', 'single'],
+		'vue/script-indent': ['warn', 'tab', {
+			'baseIndent': 1,
+			'switchCase': 1,
+			'ignores': []
+		}],
+		'vue/html-indent': ['error', 'tab', {
+			'attribute': 1,
+			'baseIndent': 1,
+			'closeBracket': 0,
+			'alignAttributesVertically': true,
+			'ignores': []
+		}]
+	},
+	overrides: [
+		{
+			'files': ['*.vue'],
+			'rules': {
+				'indent': 'off'
+			}
+		}
+	],
+};
